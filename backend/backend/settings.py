67b1df7007c3f7aacd2a298598c5dbe426c79cdb
Elena Lobichenko <lobichen@gmail.com>
2024-06-30 21:35:54 +0300

    Update secrets with host names

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,21 @@
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.76.152', '127.0.0.1', 'localhost', 'slowandsteady.redirectme.net']
-
+if os.getenv('ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+else:
+    []
 
 # Application definition
 
