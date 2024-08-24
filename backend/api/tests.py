1a91e3e597949f7fca9b0c57ad12c2b04cc602ba
Elena Lobichenko <lobichen@gmail.com>
2024-08-24 18:42:15 +0300

    Add minor cosmetic changes to the main git actions workflow and tests.

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -10,12 +10,12 @@ class TaskiAPITestCase(TestCase):
         self.guest_client = Client()
 
     def test_list_exists(self):
-        """Проверка доступности списка задач."""
+        """Проверка - запрос к списку задач возвращает HTTP-код 200."""
         response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
-        """Проверка создания задачи."""
+        """Проверка - через API можно добавить новую задачу."""
         data = {'title': 'Test', 'description': 'Test'}
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
